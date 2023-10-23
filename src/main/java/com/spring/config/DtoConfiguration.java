e737308bf935b21f2d77e688f66964a1c1cf3915
MuhammedYasserMuslim <muhmdyasser0@gmail.com>
2023-10-23 21:39:12 +0300

    Add import to config

--- src/main/java/com/spring/config/DtoConfiguration.java
+++ src/main/java/com/spring/config/DtoConfiguration.java
@@ -3,6 +3,7 @@ package com.spring.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.Scanner;
 
 @Configuration
 public class DtoConfiguration {
